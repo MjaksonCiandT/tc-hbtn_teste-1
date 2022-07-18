0725ba7bbd2ff29629c9b10ecda70b71de6489ea
Jehanlucas <jehan@ciandt.com>
2022-07-18 19:47:03 -0300

    Task 02 Teste

---
+++ 0x00/calculadora/OperacaoBasicaTeste.java
@@ -0,0 +1,38 @@
+public class OperacaoBasicaTeste {
+
+    static double resultado = OperacaoBasica.somar(5, 3);
+
+
+    public static boolean testar_soma() {
+        if (resultado == 8) {
+            return true ;
+        }
+        return false ;
+    }
+
+    public static  boolean testar_subtracao(){
+        resultado = OperacaoBasica.subtrair(5,3);
+        if (resultado ==2 ){
+            return true ;
+        }
+        return false ;
+    }
+
+    public static  boolean testar_multiplicacao(){
+        resultado = OperacaoBasica.multiplicar(5,3);
+        if (resultado == 15){
+            return true ;
+        }
+        return false ;
+    }
+    public static  boolean testar_divisao(){
+        resultado = OperacaoBasica.dividir(9,3);
+        if (resultado == 3){
+            return true ;
+        }
+        return false ;
+    }
+
+
+
+}
