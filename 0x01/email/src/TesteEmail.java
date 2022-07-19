7dfa2b0afb0e511abeb9341b644c4941184f6446
Jehanlucas <jehan@ciandt.com>
2022-07-19 13:13:07 -0300

    Testes com Junit

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -17,10 +17,10 @@ public class TesteEmail {
         assertFalse(email.emailValid("email_testedominio.com.br"));
     }
 
-    @Test
+   @Test
     public void  testar_email_mais_50_caracteres(){
         Pessoa email = new Pessoa();
-        assertFalse(email.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+        assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"),false);
 
     }
 
