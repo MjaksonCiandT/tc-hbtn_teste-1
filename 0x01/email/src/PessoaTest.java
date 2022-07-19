dbec45f80f778f01cc553a3f91197134b0ad66f4
Jehanlucas <jehan@ciandt.com>
2022-07-19 12:57:07 -0300

    Testes com Junit

---
+++ 0x01/email/src/PessoaTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PessoaTest {
+
+    @Test
+    public void  testar_email_com_arroba(){
+        Pessoa email = new Pessoa();
+        assertTrue(email.emailValid("email_teste@dominio.com.br"));
+
+    }
+
+    @Test
+    public void  testar_email_sem_arroba(){
+        Pessoa email = new Pessoa();
+        assertFalse(email.emailValid("email_testedominio.com.br"));
+    }
+
+    @Test
+    public void  testar_email_mais_50_caracteres(){
+        Pessoa email = new Pessoa();
+        assertFalse(email.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+
+    }
+
+}
