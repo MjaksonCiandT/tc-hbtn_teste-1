32e81de97f68f779cebbb4311b3bf7e877e339f6
Jehanlucas <jehan@ciandt.com>
2022-07-20 12:58:49 -0300

    Teste unitario usuario/password

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+
+    public static boolean checkUser(String usuario){
+        boolean contains = !usuario.contains("(?=.*[@#$%^&+=])");
+        if (usuario.length() >= 8 && contains){
+            return true ;
+        }
+        return false;
+    }
+
+    public static boolean checkPassword(String password){
+        if (password.matches("(^|$)[a-z]+(^|$)[0-9]")){
+            return true ;
+        }
+        return false;
+    }
+
+
+
+}
