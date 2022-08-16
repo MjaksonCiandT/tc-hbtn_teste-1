b265f86fe1a68bd520705c01fe49e8a8acfd69d2
Maikon Jakson <107194793+MjaksonCiandT@users.noreply.github.com>
2022-08-16 15:36:49 -0300

    Update TesteEmail.java

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -8,7 +8,6 @@ public class TesteEmail {
     public void  testar_email_com_arroba(){
         Pessoa email = new Pessoa();
         assertTrue(email.emailValid("email_teste@dominio.com.br"));
-
     }
 
     @Test
@@ -21,7 +20,5 @@ public class TesteEmail {
     public void  testar_email_mais_50_caracteres(){
         Pessoa email = new Pessoa();
         assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"),false);
-
     }
-
 }
